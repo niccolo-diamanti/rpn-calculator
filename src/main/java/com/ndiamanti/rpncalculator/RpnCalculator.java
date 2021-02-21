b23a05e37b34c6848c26473869247a04e69ad243
Niccolò Diamanti <niccolo.diamanti@gmail.com>
2021-02-21 23:32:19 +0100

    handle expression with variables

--- src/main/java/com/ndiamanti/rpncalculator/RpnCalculator.java
+++ src/main/java/com/ndiamanti/rpncalculator/RpnCalculator.java
@@ -5,6 +5,10 @@ import java.util.Stack;
 public class RpnCalculator {
 
     public Integer calculateExpression(String expression) {
+        if (expression.contains("|")) {
+            expression = substituteVariables(expression);
+        }
+
         Stack<Integer> values = new Stack<>();
         String[] splittedExpression = expression.split(" ");
 
@@ -15,6 +19,18 @@ public class RpnCalculator {
                 values.push(executeOperation(currentString, values));
         }
         return values.pop();
+    }
+
+    private String substituteVariables(String expression) {
+        String[] split = expression.split("\\|");
+        String variablesDefinition = split[1].trim();
+        expression = split[0].trim();
+        String[] splittedExpression = variablesDefinition.split(" ");
+        for (int i = 0; i < splittedExpression.length; i++) {
+            if (!isInteger(splittedExpression[i]))
+                expression = expression.replaceAll(splittedExpression[i], splittedExpression[i + 1]);
+        }
+        return expression;
     }
 
     private boolean isInteger(String str) {
