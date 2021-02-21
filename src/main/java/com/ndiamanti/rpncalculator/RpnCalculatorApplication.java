42c854cebdbd8697a98e73543c51744d6f13a34f
Niccolò Diamanti <niccolo.diamanti@gmail.com>
2021-02-21 18:59:56 +0100

    base project

---
+++ src/main/java/com/ndiamanti/rpncalculator/RpnCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.ndiamanti.rpncalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpnCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpnCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ndiamanti/rpncalculator/RpnCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndiamanti.rpncalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpnCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
